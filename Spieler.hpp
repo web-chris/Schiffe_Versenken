c63e9e2e50e27b320e47d307d05817e0bc3784f8
aTiger103 <136513536+aTiger103@users.noreply.github.com>
2024-01-19 13:24:19 +0100

    Update Spieler.hpp
    Umbenennung von Methodenname als Kommentare hinzugefügt

--- Spieler.hpp
+++ Spieler.hpp
@@ -21,18 +21,28 @@ private:
 
 public:
     Spieler(SpielerTyp pSpielerTyp);
-    Spieler(SpielerTyp pSpielerTyp, bool init);
+    //Spieler(SpielerTyp pSpielerTyp, bool init);
+//Umbenennen gibZellenstatusAnDerStelle
     ZellenStatus gibZellenStatus(int ireihe, int ispalte);
+//Umbenennen setzeZielZellenstatus
     void setzeZellenStatus(int ireihe, int ispalte, ZellenStatus zellenStatus);
+//Umbenennen vergleicheZelle
     bool pruefeZellen(int ireihe, int ispalte, ZellenStatus zellenStatus);
     void spielfeldAusgebe(bool schiffAnzeige);
+//Umbenennen istSchiffePlatzierenVonSpielerGueltig
     void spieler_Schiffe_platzieren(int schiffLeange, int anzahl, std::string typ);
-    bool pruefenPlatzieren(std::string eingabe, int schiffGroesse);
-    bool istGueltigeKoordinate(int reihe, int spalte);
-    int koordinateZuIndex(char buchstabe);
-    void ki_Schiffe_platzieren(int schifflaenge, int anzahl);
-    void ki_Schiffe_platzierbereit(bool platzierSchiffe);
+//Umbenennen initSchiffePlatzierungVonSpieler  bool  schiffePlatzieren
     void spieler_Schiffe_platzieren(bool platzierSchiffe);
+//Umbenennen istSchiffePlatzierenVonSpielerGueltig
+    bool pruefenPlatzieren(std::string eingabe, int schiffGroesse);
+//Umbenennen istKoordinateGültig
+    bool istGueltigeKoordinate(int reihe, int spalte);
+//Umbenennen koordinatenInIndexUmwandeln
+    int koordinateZuIndex(char buchstabe);
+//Umbenennen schiffePlatzierungVonKI
+    void ki_Schiffe_platzieren(int schifflaenge, int anzahl);
+//Umbenennen initSchiffePlatzierungVonKI    bool  schiffePlatzieren
+    void ki_Schiffe_platzierbereit(bool platzierSchiffe);
     void spielFeldInit();
     SpielFeld gibFeld();
 };
