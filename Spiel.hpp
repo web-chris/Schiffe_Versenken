79b762b75d0298cf6586aa0cab762bb247478610
aTiger103 <136513536+aTiger103@users.noreply.github.com>
2024-01-19 13:25:55 +0100

    Update Spiel.hpp
    Methodenname als Kommentare hinzugefügt

--- Spiel.hpp
+++ Spiel.hpp
@@ -10,9 +10,13 @@ public:
     bool spielerZug(std::shared_ptr<Spieler> spieler);
     bool Schuss(std::shared_ptr<Spieler> gegener, int ireihe, int ispalte);
     void KIZug(std::shared_ptr<Spieler> gegener);
+// Umbenennen istSchiffVersenkt
     void schiffversenkt(std::shared_ptr<Spieler> spieler);
+// Umbenennen sindAlleSchiffeVersenkt
     bool alleSchiffeversenkt(std::shared_ptr<Spieler> spieler);
+// Umbenennen istKoordinateGueltig
     bool istGueltigeKoordinate(int reihe, int spalte);
+// Umbenennen KoordinatenInIndexUmwandeln
     int koordinateZuIndex(char buchstabe);
     bool spielen(std::shared_ptr<Spieler> spielerSpielfeld, std::shared_ptr<Spieler> KISpielfeld, bool amZug);
 };
