f0ab5623a8a762d1beb2cb22eab4d915fc1795a7
web-chris <136088900+web-chris@users.noreply.github.com>
2024-01-19 11:48:17 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -5,10 +5,14 @@
 #include "Spieler.hpp"
 
 int main()
-{
-    Menu menu;
+{ // Spieler anlegen
     auto ki = std::make_shared<Spieler>(SpielerTyp::KI);
     auto spieler = std::make_shared<Spieler>(SpielerTyp::Spieler);
+
+    // um das Spiel zu initialisieren, wird ein Menü angelegt
+    Menu menu;
+    // hier wird das Menü für den menschlichen Spieler angezeigt
+    // und somit kann der Spieler eine der möglichen Optionen auswählen, um das Spiel beispielsweise zu starten
     menu.spielMenu(spieler, ki);
     std::cout << "\nAuf Wiedersehen!\n"<< std::endl;
 
